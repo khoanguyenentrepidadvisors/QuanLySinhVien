caced3208afb7a164beba0d684ac3d7d13dca524
khoanguyenentrepidadvisors <106294003+khoanguyenentrepidadvisors@users.noreply.github.com>
2022-05-31 08:58:43 +0700

    commit
    đã cài đặt api

---
+++ application/controllers/api/ApiSinhvienController.php
@@ -0,0 +1,14 @@
+<?php
+	defined('BASEPATH') OR exit('No direct script access allowed');
+
+	require APPPATH . 'libraries/RestController.php';
+
+	use chriskacerguis\RestServer\RestController;
+
+	class ApiSinhvienController extends RestController
+	{
+		public function index_get(){
+			echo "restful";
+		}
+	}
+?>
